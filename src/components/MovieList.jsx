0ed731bea216941bf4bd79cc4521b94bda04f3ae
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-11-20 21:16:30 +0100

    Update MovieList.jsx

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,22 @@
+import { useLocation, Link } from "react-router-dom"; 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
   return (
     <div>
       {movies.map((movie) => (
         <div key={movie.id}>
-          <img
+          <Link
+            to={`/movies/$:{movie.id}`}
+            state={{ from: location }}>
+            <img
             src={movie.poster_path
               ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
               : "https://placehold.co/160x260"
             }
-            alt={movie.title} />
-          <h3>{movie.title}</h3>
+              alt={movie.title}
+            />
+            <h3>{movie.title}</h3>
+          </Link>
         </div>
       ))}
     </div>
