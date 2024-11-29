4f3e22838844d330ecfcee125fa77420071784b1
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-11-29 14:19:54 +0100

    .

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import css from "../css/NotFoundPage.module.css"
 
 export default function NotFoundPage() {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, something went wrong. Please check URL of page you wanted to go.</p>
+      <h1 className={css.notFound}>404 - Page Not Found</h1>
+      <p className={css.adnotation}>Sorry, something went wrong. Please check URL of page you wanted to go.</p>
       <Link to="/">
         <button>Home site</button>
       </Link>
